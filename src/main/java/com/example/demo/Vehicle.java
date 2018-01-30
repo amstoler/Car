1697fd4da1f9736772e8210929631a6a9963b175
Ariel Stoler <amstoler@gmail.com>
2018-01-30 11:06:12 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Vehicle.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class MainController {
+    private String  name;
+    private String model;
+    private 
+    public MainController() { System.out.println("A new vehicle has been created!");}
+
+    public String accelerate() {return "The red porsche is accelerating.";}
+    public String start() {return " The red porsche is starting";}
+}
